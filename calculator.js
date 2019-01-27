e0c7c0d7ca8a824a2ca40d6b9fe6b759cbb6b9eb
Philippe Donaus <philippe.donaus@gmail.com>
2019-01-27 11:44:49 -0500

    Creates changes to JS file

--- calculator.js
+++ calculator.js
@@ -1,1 +1,36 @@
-calculator.js
+
+var x = 'X'
+var o = 'O'
+var values = {
+	1:'',
+	2:'',
+	3:'',
+	4:'',
+	5:'',
+	6:'',
+	7:'',
+	8:'',
+	9:''
+}
+
+
+var current = ''
+
+function change_status(state){
+	if (state === 'X'){
+		return '0';
+	}
+	else{
+		return 'X';
+	}
+}
+
+function onclick(index){
+
+	current = change_status(values[index])
+	values[index] = current
+	return current
+}
+
+
+inse
